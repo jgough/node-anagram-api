a49ccbfe100593d72286387161ecff8defc05a24
Jonathan Gough <jonathanpgough@gmail.com>
2016-05-12 10:33:49 +0100

    Updating paths

--- load-data.sh
+++ load-data.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 wget -N -P downloads http://parlvid.mysociety.org/os/ONSPD_FEB_2016_csv.zip
-docker-compose run web node /src/app/bin/www/load_data.js
+docker-compose run web node /src/load_data.js
